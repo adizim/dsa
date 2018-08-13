f3e9ec9ff526a55d037b4d3d8288696359e1c489
Adi <adizim@berkeley.edu>
2018-08-13 09:09:28 -0700

    tree struct

--- ds/src/tree.py
+++ ds/src/tree.py
@@ -2,3 +2,8 @@ class Tree:
 	def __init__(self, data=0, children=[]):
 		self.data = data
 		self.children = children
+
+class BinaryTree:
+	def __init__(self, data=0, children=[]):
+		self.data = data
+		
